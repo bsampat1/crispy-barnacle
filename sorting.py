bf2fa3a512a2437b00c9732ef769012809cbf466
bsampat1 <balaji.aws@gmail.com>
2016-02-28 00:01:20 -0500

    add timing and track swaps in bubble_sort

--- sorting.py
+++ sorting.py
@@ -2,6 +2,10 @@
 """
    Various types of sorting
 """
+import random
+import timeit
+
+test_list = random.sample(range(-5, 10), 10)
 
 
 def bubble_sort(_list):
@@ -11,10 +15,29 @@ def bubble_sort(_list):
                 _list[i], _list[j] = _list[j], _list[i]
     return _list
 
+
+def improve_bubble_sort(_list):
+    for i in range(len(_list)):
+        swaps = 0
+        for j in range(i, len(_list)):
+            if _list[i] > _list[j]:
+                _list[i], _list[j] = _list[j], _list[i]
+                swaps += 1
+            if swaps == 0:
+                break
+    return _list
+
+
 if __name__ == '__main__':
-    import random
-    test_list = random.sample(range(-5, 10), 10)
     print "Unsorted:"
     print test_list
     print "Sorted by Bubble sort:"
     print bubble_sort(test_list)
+    print "time for basic bubble sort"
+    t = timeit.Timer(stmt="sorting.bubble_sort(sorting.test_list)", setup="import sorting")
+    print t.timeit(5)
+    print "Sorted by Improved Bubble sort:"
+    print improve_bubble_sort(test_list)
+    print "time for improved bubble sort"
+    t = timeit.Timer(stmt="sorting.improve_bubble_sort(sorting.test_list)", setup="import sorting")
+    print t.timeit(5)
